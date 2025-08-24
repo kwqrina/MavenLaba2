08998376b7a82978579248bab175eb1f94cd7679
kwqrina <ekaterina.motruk@gmail.com>
2025-08-24 14:55:12 +0300

    1

--- mavenlaba2/src/main/java/controller/ArmyController.java
+++ mavenlaba2/src/main/java/controller/ArmyController.java
@@ -44,7 +44,7 @@ public class ArmyController {
 
     public void launchApplication() {          //метод запуска графического интерфейса
         SwingUtilities.invokeLater(() -> {
-            this.view = new SayronArmyGui(this);
+            this.view = new SayronArmyGui();
             view.setVisible(true);
         });
     }
